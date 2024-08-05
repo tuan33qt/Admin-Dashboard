9bba68372eb24e24ae6dd55887b1a52406ce5fd1
tuan <tuan33qt@gmail.com>
2024-08-05 21:29:34 +0700

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 import Booking from "./pages/Booking";
-import Checkin from "./pages/CheckIn";
+import Checkin from "./pages/Checkin";
 import ProtectedRoute from "./ui/ProtectedRoute";
 const queryClient = new QueryClient({
   defaultOptions: {
